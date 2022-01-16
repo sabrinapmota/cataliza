9526883344b770f483e6acae264a0ae675706927
sabrina porto <sabrinapmota@yahoo.com.br>
2022-01-15 21:49:15 -0300

    capturar elementos html manipuláveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const img = document.querySelector('img';)
+const botao = document.querySelector('button');
+const nomeDOPersonagem = document.querySelector('#id=nome');
+const espécie = document.querySelector(#id = 'specie');
+const condição=document.querySelector (#id='condicao');
